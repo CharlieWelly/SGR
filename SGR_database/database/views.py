0ad5a2127d5bd9221f8636bef471ca21ec4f0ed0
Cuong Tran Chi <VNTCC@ptr.com>
2021-06-30 15:40:43 +0700

    create login and layout templates

--- SGR_database/database/views.py
+++ SGR_database/database/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    return render(request, "database/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        user = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=user, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "database/login.html")
+    return render(request, "database/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
